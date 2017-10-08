7863753e0beef800705f27215a59808609824348
ianfeng <fyl031020@126.com>
2017-10-08 11:17:10 +0800

    first commit by ian

---
+++ test01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string newstring = "A String";
+            foreach (char item in newstring)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("the length of the string is {0}", newstring.Length);
+            Console.WriteLine("the upper string is {0}", newstring.ToUpper());
+            Console.WriteLine("the lower string is {0}", newstring.ToLower());
+            Console.ReadKey();
+        }
+    }
+}
